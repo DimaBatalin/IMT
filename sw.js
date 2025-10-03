62b5baadb179d4bf869751bdbed86f3e7ef63a1b
DimaBatalin <125128471+DimaBatalin@users.noreply.github.com>
2025-10-03 10:45:43 +0500

    www

---
+++ sw.js
@@ -0,0 +1,37 @@
+const staticCacheName = 'site-static-v1'
+const assets = [
+    '.',
+    'main.html',
+    'images/img.png',
+    'css/style.css',
+    'manifest_and_icons/icon512_maskable.png',
+    'manifest_and_icons/icon512_rounded.png'
+]
+
+self.addEventListener('install', evt => {
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('assets')
+            cache.addAll(assets)
+        })
+    )
+})
+
+self.addEventListener('activate', evt => {
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== staticCacheName)
+                    .map(key => caches.delete(key))
+            )
+        })
+    )
+})
+
+self.addEventListener('fetch', evt => {
+    evt.respondWith(
+        caches.match(evt.request).then(cache => {
+            return cache || fetch(evt.request)
+        })
+    )
+})
